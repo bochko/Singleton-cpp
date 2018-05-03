21dbbba8f13ec9b5041cd80a5c596a1c08e8220b
Boyan Atanasov <b.atanasov@icloud.com>
2018-05-03 23:37:31 +0100

    Implemented thread-safe singleton with mutex lock.

--- singleton/singleton.cpp
+++ singleton/singleton.cpp
@@ -1,7 +1,9 @@
 #include "singleton.hpp"
 
-bochko::patterns::singleton* bochko::patterns::singleton::instance = nullptr;
-
+/*
+ * class: singleton
+ */
+bochko::patterns::singleton *bochko::patterns::singleton::instance = nullptr;
 bochko::patterns::singleton::singleton() = default;
 
 bochko::patterns::singleton::~singleton() {
@@ -16,4 +18,24 @@ bochko::patterns::singleton *bochko::patterns::singleton::get_instance() {
 	return bochko::patterns::singleton::instance;
 }
 
+/*
+ * class: mutable_singleton
+ */
+bochko::patterns::mutable_singleton *bochko::patterns::mutable_singleton::instance = nullptr;
+std::mutex bochko::patterns::mutable_singleton::mutex;
+bochko::patterns::mutable_singleton::mutable_singleton() = default;
+
+bochko::patterns::mutable_singleton::~mutable_singleton() {
+	if (bochko::patterns::mutable_singleton::instance == nullptr) return;
+	delete bochko::patterns::mutable_singleton::instance;
+}
+
+bochko::patterns::mutable_singleton *bochko::patterns::mutable_singleton::get_instance() {
+	mutex.lock();
+	if (bochko::patterns::mutable_singleton::instance == nullptr) {
+		bochko::patterns::mutable_singleton::instance = new mutable_singleton();
+	}
+	mutex.unlock();
+	return bochko::patterns::mutable_singleton::instance;
+}
 
--- singleton/singleton.hpp
+++ singleton/singleton.hpp
@@ -1,6 +1,8 @@
 #ifndef BOCHKO_UTILS_SINGLETON_HPP
 #define BOCHKO_UTILS_SINGLETON_HPP
 
+#include <mutex>
+
 namespace bochko {
 	namespace patterns {
 		/**
@@ -9,17 +11,28 @@ namespace bochko {
 		class singleton {
 		private:
 			static singleton *instance;
-
 			singleton();
-
 			~singleton();
-
 		public:
 			singleton(const singleton&) = delete;
-
 			singleton& operator=(const singleton&) = delete;
-
 			static singleton *get_instance();
+		};
+
+		/**
+		 * Classic thread-safe, mutex-ed singleton.
+		 */
+		class mutable_singleton {
+		private:
+			static mutable_singleton *instance;
+			mutable_singleton();
+			~mutable_singleton();
+		protected:
+			static std::mutex mutex;
+		public:
+			mutable_singleton(const mutable_singleton&) = delete;
+			mutable_singleton& operator=(const mutable_singleton&) = delete;
+			static mutable_singleton *get_instance();
 		};
 	}
 }
